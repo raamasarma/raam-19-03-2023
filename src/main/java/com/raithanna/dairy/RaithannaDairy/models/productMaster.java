2188cbc6b7bf12265847a72d87ecfc735126bedf
Bitbucket Expert <ramasarma.pakala@gmail.com>
2023-03-19 18:49:34 +0530

    first commit

--- src/main/java/com/raithanna/dairy/RaithannaDairy/models/productMaster.java
+++ src/main/java/com/raithanna/dairy/RaithannaDairy/models/productMaster.java
@@ -1,5 +1,4 @@
 package com.raithanna.dairy.RaithannaDairy.models;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,45 +14,42 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Map;
-
 @Entity
 @Table
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class productMaster {
+public class productMaster implements Comparable{
     @Id
     @GeneratedValue
     private int id;
-
     private String PCode;
     private String PName;
     private String prod_type;
-    private Date PStDate;
-    private Date PEndDate;
-    private double unitRate;
-    private double qtyFrom;
-    private double qtyto;
+    private String PStDate;
+    private String PEndDate;
+    private Double unitRate;
+    private Double qtyFrom;
+    private Double qtyto;
     private String active;
     private String remove;
     private String sourceData;
     private String prodLabel;
-    private double milkUnits;
+    private Double milkUnits;
     private String splCustCode;
     private String cont;
     private String UOM;
     private String HSN;
     private String prodQ;
     private String qtyL;
-    private double gstPercent;
-    private Date GStDate;
-    private Date GEndDate;
-
+    private Double gstPercent;
+    private String GStDate;
+    private String GEndDate;
+    private String flag;
     public void mapToVariables(@NotNull Map product) throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
         // Int
         this.id = Integer.parseInt( product.get("id").toString());
-
         // Strings
         this.PCode = product.get("PCode").toString();
         this.PName = product.get("PName").toString();
@@ -65,20 +61,29 @@ public class productMaster {
         this.UOM = product.get("UOM").toString();
         this.HSN = product.get("HSN").toString();
         this.splCustCode = product.get("splCustCode").toString();
-
         // Doubles
         this.unitRate = Double.parseDouble(product.get("unitRate").toString());
         this.qtyFrom = Double.parseDouble(product.get("qty_from").toString());
         this.qtyto = Double.parseDouble(product.get("qty_to").toString());
         this.milkUnits = Double.parseDouble(product.get("milkUnits").toString());
         this.gstPercent = Double.parseDouble(product.get("gstPercent").toString());
-
         // Dates
-        this.PStDate = formatter.parse(product.get("PStDate").toString());
-        this.PEndDate = formatter.parse(product.get("PEndDate").toString());
+      //  this.PStDate = formatter.parse(product.get("PStDate").toString());
+       // this.PEndDate = formatter.parse(product.get("PEndDate").toString());
 //        this.GStDate = DateFormat.getDateInstance().parse(product.get("GStDate").toString());
 //        this.GEndDate = DateFormat.getDateInstance().parse(product.get("GEndDate").toString());
-
     }
 
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public int compareTo(@NotNull Object o) {
+        return 0;
+    }
 }
